ace597cff13c5be04071761db7fe9e3a7897e354
KMtouzhele <135633869+KMtouzhele@users.noreply.github.com>
2025-03-09 21:40:28 +1100

    frontend: add setting page

---
+++ budget_frontend/src/app/budget/settings/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import {
+    Box,
+} from '@mui/material';
+
+export default function ExpensesPage() {
+    return (
+        <Box>
+            Settings Page
+        </Box>
+    );
+}
