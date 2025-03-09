19bb1539804b60759647334c4c2dd48775ecfee8
KMtouzhele <135633869+KMtouzhele@users.noreply.github.com>
2025-03-09 14:44:32 +1100

    PUT and DELETE requests are supported in expenses api

--- budget_api/Controllers/ExpenseController.cs
+++ budget_api/Controllers/ExpenseController.cs
@@ -126,5 +126,134 @@ namespace Budget.Controllers
                 }
             );
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpense(int id, ExpenseModel model)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return Unauthorized(
+                    new
+                    {
+                        status = 401,
+                        title = "User not authenticated",
+                        message = "User not authenticated"
+                    }
+                );
+            }
+
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound(
+                    new
+                    {
+                        status = 404,
+                        title = "Expense not found",
+                        message = "Expense not found"
+                    }
+                );
+            }
+
+            if (expense.UserId != userId)
+            {
+                return Unauthorized(
+                    new
+                    {
+                        status = 401,
+                        title = "User not authorized",
+                        message = "User not authorized to update this expense"
+                    }
+                );
+            }
+
+            if (!Enum.TryParse<ExpenseCategory>(model.Category, true, out var expenseCategory) ||
+            !Enum.IsDefined(expenseCategory))
+            {
+                return BadRequest(
+                new
+                {
+                    status = 400,
+                    title = "Invalid expense category",
+                    message = "Needed to be one of: Food, Rent, Bills, Transport, Other"
+                }
+                );
+            }
+
+            expense.Name = model.Name;
+            expense.Category = model.Category;
+            expense.Description = model.Description;
+            expense.Amount = model.Amount;
+            expense.Currency = model.Currency;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(
+                new
+                {
+                    status = 200,
+                    title = "Expense updated successfully",
+                    message = new
+                    {
+                        expense = expense
+                    },
+                }
+            );
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return Unauthorized(
+                    new
+                    {
+                        status = 401,
+                        title = "User not authenticated",
+                        message = "User not authenticated"
+                    }
+                );
+            }
+
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound(
+                    new
+                    {
+                        status = 404,
+                        title = "Expense not found",
+                        message = "Expense not found"
+                    }
+                );
+            }
+
+            if (expense.UserId != userId)
+            {
+                return Unauthorized(
+                    new
+                    {
+                        status = 401,
+                        title = "User not authorized",
+                        message = "User not authorized to delete this expense"
+                    }
+                );
+            }
+
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+
+            return Ok(
+                new
+                {
+                    status = 200,
+                    title = "Expense deleted successfully",
+                    message = "Expense deleted successfully"
+                }
+            );
+        }
     }
 }
